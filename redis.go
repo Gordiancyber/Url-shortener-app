14f2b19b065b266ce8727372fba631cc0065e15c
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-08-21 11:08:28 +0100

    Update redis.go

--- redis.go
+++ redis.go
@@ -5,14 +5,16 @@ import (
     "github.com/go-redis/redis/v8"
 )
 
+// Create a context.Background() instance to be used globally
 var ctx = context.Background()
 
+// NewRedisClient initializes and returns a new Redis client instance.
 func NewRedisClient() *redis.Client {
+    // Create a new Redis client with the specified options
     rdb := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "",
-        DB:       0,
+        Addr:     "localhost:6379", // Address of the Redis server
+        Password: "",               // Redis server password (empty if not required)
+        DB:       0,                // Database index to use
     })
 
-    return rdb
-}
+    // Return the
